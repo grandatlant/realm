8cbd7b462ecd34c69137cec19fd0368af92d99bc
grandatlant <grandatlant@gmail.com>
2025-01-05 06:24:54 +0200

    Initial

---
+++ realm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+"""
+Realm changing script for World of Warcraft
+"""
+
+import sys
+
+
+
+def main(argv=None):
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
