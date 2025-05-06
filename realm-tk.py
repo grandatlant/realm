404e2bf4877a2f319ce78329a4d8d565727dd55c
grandatlant <grandatlant@gmail.com>
2025-05-06 18:31:55 +0300

    Template for start

--- realm-tk.py
+++ realm-tk.py
@@ -7,9 +7,21 @@ using tkinter
 import sys
 import tkinter as tk
 
+
+def build_ui(parent):
+    frame = tk.Frame(parent, relief='ridge', borderwidth=5)
+    frame.pack(fill='both', expand=1)
+    
+    label = tk.Label(frame, text="--- Main frame for UI ---")
+    label.pack(fill='x', expand=1)
+    
+    button = tk.Button(frame, text="Exit", command=parent.destroy)
+    button.pack(side='bottom')
+
 ##  MAIN ENTRY POINT
 def main(args=None):
     root = tk.Tk()
+    build_ui(root)
     root.mainloop()
     return 0
 
