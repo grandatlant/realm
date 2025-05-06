0975a732e5b54b3a488dbd24db03ec4d4b9a792f
grandatlant <grandatlant@gmail.com>
2025-05-06 17:09:48 +0300

    New file - tkinter gui entry point

---
+++ realm-tk.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env -S python3 -OO
+# -*- coding = utf-8 -*-
+"""
+Realm changing module for World of Warcraft,
+using tkinter
+"""
+import sys
+import tkinter as tk
+
+##  MAIN ENTRY POINT
+def main(args=None):
+    root = tk.Tk()
+    root.mainloop()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
